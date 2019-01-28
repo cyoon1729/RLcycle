bc0d56f6c32263e20e0df5fa853f7477734a61ad
Christopher Yoon <thechrisyoon08@gmail.com>
2019-01-28 01:28:20 -0500

    a2c before debugging

--- A2C/a2c.py
+++ A2C/a2c.py
@@ -12,6 +12,9 @@ hidden_size = 256
 lr = 3e-4
 num_steps = 5
 
+# Constants
+GAMMA = 0.9
+
 class PolicyNetwork(nn.Module):
     def __init__(self, num_inputs, num_actions, hidden_size, learning_rate=3e-4):
         super(PolicyNetwork, self).__init__()
@@ -59,16 +62,52 @@ def a2c(env):
         values = []
         rewards = []
 
-        action, log_prob = policy_network.get_action(state)
-        value = value_network.forward(state)
-        new_state, reward, done, _ = env.step(action)
+        for steps in range(num_steps):
+            action, log_prob = policy_network.get_action(state)
+            value = value_network.forward(state)
+            new_state, reward, done, _ = env.step(action)
 
-        log_probs.append(log_prob)
-        rewards.append(reward)
-        values.append(value)
+            log_probs.append(log_prob)
+            rewards.append(reward)
+            values.append(value)
 
-        state = new_state
+            state = new_state
         
+    
+    discounted_rewards = []
+    for t in range(len(rewards)):
+        Gt = 0 
+        pw = 0
+        for r in rewards[t:]:
+            Gt = Gt + GAMMA**pw * r
+            pw = pw + 1
+        discounted_rewards.append(Gt)
+    
+    policy_gradient = []
+    value_loss = []
+    for log_prob, returns, value in zip(log_probs, discounted_rewards, values):
+        advantage = returns - value
+        policy_gradient.append(-log_prob * advantage)
+        critic_loss.append(advantage.pow(2))
+    
+    # Actor update
+    policy_network.optimizer.zero_grad()
+    policy_gradient = torch.stack(policy_gradient).sum().mean()
+    policy_gradient.backward()
+    policy_network.optimizer.step()
+
+    # Critic update
+    value_network.optimizer.zero_grad()
+    value_loss = torch.stack(value_loss).sum().mean()
+    value_network.backward()
+    value_network.optimizer.step()
+
+if __name__ == "__main__":
+    env = gym.make("Cartpole-v0")
+    a2c(env)
+
+
+    
 
 
 
