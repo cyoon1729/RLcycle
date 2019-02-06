38475d786a49df7a5a202efd12c243e11601399f
Christopher Yoon <thechrisyoon08@gmail.com>
2019-02-06 02:35:14 -0500

    fix a2x - now working

--- A2C/a2c.py
+++ A2C/a2c.py
@@ -7,15 +7,16 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # hyperparameters
 hidden_size = 256
 learning_rate = 3e-4
 
 # Constants
-GAMMA = 0.9
-num_steps = 1000
-max_episodes = 2500
+GAMMA = 0.99
+num_steps = 300
+max_episodes = 3000
 
 class ActorCritic(nn.Module):
     def __init__(self, num_inputs, num_actions, hidden_size, learning_rate=3e-4):
@@ -45,11 +46,11 @@ def a2c(env):
     num_outputs = env.action_space.n
     
     actor_critic = ActorCritic(num_inputs, num_outputs, hidden_size)
-    value_criterion = nn.MSELoss()
     ac_optimizer = optim.Adam(actor_critic.parameters(), lr=learning_rate)
 
     all_lengths = []
     average_lengths = []
+    all_rewards = []
     
     for episode in range(max_episodes):
         log_probs = []
@@ -67,27 +68,26 @@ def a2c(env):
             state = new_state
             
             if done:
+                Qval, _, _ = actor_critic.forward(new_state)
+                all_rewards.append(np.sum(rewards))
                 all_lengths.append(steps)
                 average_lengths.append(np.mean(all_lengths[-10:]))
                 if episode % 10 == 0:                    
-                    sys.stdout.write("episode: {}, total length: {}, average length: {} \n".format(episode, steps, average_lengths[-1]))
+                    sys.stdout.write("episode: {}, reward: {}, total length: {}, average length: {} \n".format(episode, np.sum(rewards), steps, average_lengths[-1]))
                 break
         
-        discounted_rewards = []
-        for t in range(len(rewards)):
-            Gt = 0 
-            pw = 0
-            for r in rewards[t:]:
-                Gt = Gt + GAMMA**pw * r
-                pw = pw + 1
-            discounted_rewards.append(Gt)
-
+        # compute Q values
+        Qvals = []
+        for t in reversed(range(len(rewards))):
+            Qval = rewards[t] + GAMMA * Qval
+            Qvals.insert(0, Qval) 
+  
         #update actor critic
         values = torch.stack(values)
-        discounted_rewards = torch.Tensor(discounted_rewards)
+        Qvals = torch.Tensor(Qvals)
         log_probs = torch.stack(log_probs)
         
-        advantage = discounted_rewards - value
+        advantage = Qvals - value
         actor_loss = (-log_probs * advantage).mean()
         critic_loss = 0.5 * advantage.pow(2).mean()
         ac_loss = actor_loss + critic_loss
@@ -95,12 +95,24 @@ def a2c(env):
         ac_optimizer.zero_grad()
         ac_loss.backward()
         ac_optimizer.step()
+    
+    # Plot results
+    smoothing_rewards = pd.Series(all_rewards)
+    smoothing_rewards = pd.Series.rolling(smoothing_rewards, 10).mean()
+    smoothend_rewards = [elem for elem in smoothing_rewards]
+    plt.plot(all_rewards)
+    plt.plot(smoothend_rewards)
+    plt.plot()
+    plt.xlabel('Episode')
+    plt.ylabel('Reward')
+    plt.show()
 
     plt.plot(all_lengths)
     plt.plot(average_lengths)
     plt.xlabel('Episode')
     plt.ylabel('Episode length')
     plt.show()
+
     
     
 if __name__ == "__main__":
