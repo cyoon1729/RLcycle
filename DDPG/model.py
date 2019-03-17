3659b323f505023b159b1683cb4bbeb06ed76b47
Christopher Yoon <thechrisyoon08@gmail.com>
2019-03-17 13:48:57 -0400

    move numpy->torch to outside

--- DDPG/model.py
+++ DDPG/model.py
@@ -2,23 +2,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F 
 import torch.autograd
-import torch.optim as optim
+from torch.autograd import Variable
 
 class Critic(nn.Module):
-
-    def __init__(self, input_size, hidden_size, output_size, init_w = 3e-3):
-        super(Critic, self)__init__()
+    def __init__(self, input_size, hidden_size, output_size, init_w=3e-3):
+        super(Critic, self).__init__()
 
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, hidden_size)
         self.linear3 = nn.Linear(hidden_size, output_size)
 
-        self.linear3.weight.data_uniform_(-init_w, init_w)
-        self.linear3.bias.data_uniform_(-init_w, init_w)
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
     
     def forward(self, state, action):
-        state = Variable(torch.from_numpy(state).float().unsqueeze(0))
-        action = Variable(torch.from_numpy(action).float().unsqueeze(0))
+        """
+        state and actions are torch tensors
+        """
+        #state = Variable(torch.from_numpy(state).float().unsqueeze(0))
+        #action = Variable(torch.from_numpy(action).float().unsqueeze(0))
         
         x = torch.cat([state, action], 1)
         x = F.relu(self.linear1(x))
@@ -28,25 +30,24 @@ class Critic(nn.Module):
         return x
 
 class Actor(nn.Module):
-
-    def __init__(self, input_size, hidden_size, output_size, learning_rate = 3e-4):
-        super(Actor, self)__init__()
+    def __init__(self, input_size, hidden_size, output_size, learning_rate = 3e-4, init_w=3e-3):
+        super(Actor, self).__init__()
 
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, hidden_size)
         self.linear3 = nn.Linear(hidden_size, output_size)
         
-        self.linear3.weight.data_uniform_(-init_w, init_w)
-        self.linear3.bias.data_uniform_(-init_w, init_w)
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
         
-    def forward(self, x):
-        x = Variable(torch.from_numpy(x).float().unsqueeze(0))
+    def forward(self, state):
+        """
+        state is a torch tensor
+        """
+        #x = Variable(torch.from_numpy(state).float().unsqueeze(0))
+        x = F.relu(self.linear1(state))
         x = F.relu(self.linear2(x))
-        x = F.tanh(self.linear3(x))
+        x = torch.tanh(self.linear3(x))
 
         return x
 
-    def get_action(self, state):
-        state = Variable(torch.from_numpy(state).float().unsqueeze(0))
-        action = self.forward(state).detach().cpu().numpy()[0, 0]
-        
