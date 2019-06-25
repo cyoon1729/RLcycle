62d8062637a609a6ae2463a3b2f9fdcd512ad3d2
chris <chrisyoon1729@gmail.com>
2019-06-25 01:04:44 -0400

    fix distribution projection

--- common/utils.py
+++ common/utils.py
@@ -7,46 +7,25 @@ def KL_divergence(p_mean, p_std, q_mean, q_std):
     return kld
 
 # project value distribution onto atoms as in Categorical Algorithm
-def dist_projection(n_atoms, Vmin, Vmax, batch, next_best_dist, gamma):
-    """
-    Params:
-        n_atoms (int)
-        Vmin (float)
-        Vmax (float)
-        batch (array of experience tuples)
-        next_dist (tensor)
-        gamma (float)
-    """
-
-    batch_size = batch.size(0)
-    states, actions, rewards, next_states, dones = batch
-    # next_best_dist = next_dist[range(batch_size), next_actions]
-
+def dist_projection(optimal_dist, rewards, dones, gamma, n_atoms, Vmin, Vmax, support):
+    batch_size = rewards.size(0)
+    m = torch.zeros(batch_size, n_atoms)
     delta_z = (Vmax - Vmin) / (n_atoms - 1)
-    m = torch.zeros_like(next_dist)
 
     for sample_idx in range(batch_size):
-        action = actions[sample_idx].long.item()
         reward = rewards[sample_idx]
         done = dones[sample_idx]
 
-
-        for atom_dix in range(n_atoms):
-            # Compute projection of Tz_j onto {z_i}
-            Tz_j = reward + (1 - done) * gamma * (Vmin + i * delta_z)
+        for atom in range(n_atoms):
+            # compute projection of Tz_j
+            Tz_j = reward + (1 - done) * gamma * support[atom]
             Tz_j = torch.clamp(Tz_j, Vmin, Vmax)
             b_j = (Tz_j - Vmin) / delta_z
-
             l = torch.floor(b_j).long().item()
             u = torch.ceil(b_j).long().item()
 
-            p_j = next_dist[sample_idx, action]
-            # Distribute probability of Tz_j
-            if done:
-                m[sample_idx, action, l] = m[sample_idx, action, l] + (u - b_j)
-                m[sample_idx, action, u] = m[sample_idx, action, u] + (b_j - l)
-            else:
-                m[sample_idx, action, l] = m[sample_idx, action, l] + next_best_dist[sample_idx] * (u - b_j)
-                m[sample_idx, action, u] = m[sample_idx, action, u] + next_best_dist[sample_idx]* (b_j - l)
+            # distribute probability of Tz_j
+            m[sample_idx][l] = m[sample_idx][l] + optimal_dist[sample_idx][atom] * (u - b_j)
+            m[sample_idx][u] = m[sample_idx][u] + optimal_dist[sample_idx][atom] * (b_j - l)
 
-
+    return m
