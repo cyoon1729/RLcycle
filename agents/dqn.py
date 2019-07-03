da1f5d0bb374a5c1f95018cd8c18186a72be97e3
chris <chrisyoon1729@gmail.com>
2019-07-03 16:44:20 -0400

    separate no conv and conv

--- agents/dqn.py
+++ agents/dqn.py
@@ -3,9 +3,64 @@ import torch.nn as nn
 import torch.autograd as autograd
 import numpy as np
 
-from common.models import VanillaDQN
 from common.replay_buffers import BasicBuffer
 
+
+class ConvDQN(nn.Module):
+    
+    def __init__(self, input_dim, output_dim):
+        super(ConvDQN, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.fc_input_dim = self.feature_size()
+        
+        self.conv = nn.Sequential(
+            nn.Conv2d(self.input_dim[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(self.fc_input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Linear(256, self.output_dim)
+        )
+
+    def forward(self, state):
+        features = self.conv_net(state)
+        features = features.view(features.size(0), -1)
+        qvals = self.fc(features)
+        return qvals
+
+    def feature_size(self):
+        return self.conv_net(autograd.Variable(torch.zeros(1, *self.input_dim))).view(1, -1).size(1)
+
+
+class DQN(nn.Module):
+    
+    def __init__(self, input_dim, output_dim):
+        super(DQN, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        
+        self.fc = nn.Sequential(
+            nn.Linear(self.input_dim[0], 128),
+            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Linear(256, self.output_dim)
+        )
+
+    def forward(self, state):
+        qvals = self.fc(state)
+        return qvals
+
+    
 class DQNAgent:
 
     def __init__(self, env, use_conv=True, learning_rate=3e-4, gamma=0.99, buffer_size=10000):
@@ -48,4 +103,3 @@ class DQNAgent:
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
-
--- agents/dueling_ddqn.py
+++ agents/dueling_ddqn.py
@@ -3,8 +3,83 @@ import torch.nn as nn
 import torch.autograd as autograd
 import numpy as np
 
-from common.models import DuelingDQN
 from common.replay_buffers import BasicBuffer
+
+
+class ConvDuelingDQN(nn.Module):
+
+    def __init__(self, input_dim, output_dim):
+        super(ConvDuelingDQN, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.fc_input_dim = self.feature_size()
+        
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_dim[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.value_stream = nn.Sequential(
+            nn.Linear(self.fc_input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+        self.advantage_stream = nn.Sequential(
+            nn.Linear(self.fc_input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, self.output_dim)
+        )
+
+    def forward(self, state):
+        features = self.conv(state)
+        features = features.view(features.size(0), -1)
+        values = self.value_stream(features)
+        advantages = self.advantage_stream(features)
+        qvals = values + (advantages - advantages.mean())
+        
+        return qvals
+
+    def feature_size(self):
+        return self.conv_net(autograd.Variable(torch.zeros(1, *self.input_dim))).view(1, -1).size(1)
+
+
+class DuelingDQN(nn.Module):
+
+    def __init__(self, input_dim, output_dim):
+        super(DuelingDQN, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.fc_input_dim = self.feature_size()
+        
+        self.feauture_layer = nn.Sequential(
+            nn.Linear(self.input_dim[0], 128),
+            nn.ReLU()
+        )
+
+        self.value_stream = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+        self.advantage_stream = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, self.output_dim)
+        )
+
+    def forward(self, state):
+        features = self.feauture_layer(state)
+        values = self.value_stream(features)
+        advantages = self.advantage_stream(features)
+        qvals = values + (advantages - advantages.mean())
+        
+        return qvals
 
 
 class DuelingAgent:
@@ -14,7 +89,12 @@ class DuelingAgent:
         self.learning_rate = learning_rate
         self.gamma = gamma
         self.replay_buffer = BasicBuffer(max_size=buffer_size)
-        self.model = DuelingDQN(env.observation_space.shape, env.action_space.n, use_conv)
+        
+        if self.use_conv:
+            self.model = ConvDuelingDQN(env.observation_space.shape, env.action_space.n, use_conv)
+        else:
+            self.model = DuelingDQN(env.observation_space.shape, env.action_space.n, use_conv)
+        
         self.optimizer = torch.optim.Adam(self.model.parameters())
         self.MSE_loss = nn.MSELoss()
 
@@ -49,5 +129,3 @@ class DuelingAgent:
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
-
-
