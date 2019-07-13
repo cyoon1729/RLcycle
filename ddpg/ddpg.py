ff3992cd146df00beb182b2d300d8bb4159730c7
chris <chrisyoon1729@gmail.com>
2019-07-13 13:40:30 -0400

    minor refactoring

--- ddpg/ddpg.py
+++ ddpg/ddpg.py
@@ -1,8 +1,9 @@
 import torch
 import torch.optim as optim
 import torch.autograd as autograd
+import torch.nn.functional as F
 
-from ddpg.models import Critic, Actor
+from models import Critic, Actor
 from common.replay_buffers import BasicBuffer
 from common.noise import OUNoise
 
@@ -10,18 +11,18 @@ from common.noise import OUNoise
 class DDPGAgent:
     
     def __init__(self, env, gamma, tau, buffer_maxlen, critic_learning_rate, actor_learning_rate):
-        self.env = env
-        self.noise = OUNoise(self.env.action_space)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
+        self.env = env
+        self.obs_dim = env.observation_space.shape[0]
+        self.action_dim = env.action_space.shape[0]
+        
+        # hyperparameters
         self.env = env
         self.gamma = gamma
         self.tau = tau
         
-        self.obs_dim = env.observation_space.shape[0]
-        self.action_dim = env.action_space.shape[0]
-        
-	self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            
+        # initialize actor and critic networks
         self.critic = Critic(self.obs_dim, self.action_dim).to(self.device)
         self.critic_target = Critic(self.obs_dim, self.action_dim).to(self.device)
         
@@ -32,13 +33,15 @@ class DDPGAgent:
         for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
             target_param.data.copy_(param.data)
         
-        self.replay_buffer = BasicBuffer(buffer_maxlen)        
-        self.MSELoss  = nn.MSELoss()
+        # optimizers
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=critic_learning_rate)
         self.actor_optimizer  = optim.Adam(self.actor.parameters(), lr=actor_learning_rate)
     
+        self.replay_buffer = BasicBuffer(buffer_maxlen)        
+        self.noise = OUNoise(self.env.action_space)
+        
     def get_action(self, obs):
-        state = autograd.Variable(torch.FloatTensor(obs).unsqueeze(0)).to(self.device)
+        state = torch.FloatTensor(obs).unsqueeze(0).to(self.device)
         action = self.actor.forward(state)
         action = action.squeeze(0).cpu().detach().numpy()
 
@@ -53,20 +56,22 @@ class DDPGAgent:
         next_state_batch = torch.FloatTensor(next_state_batch).to(self.device)
         masks = torch.FloatTensor(masks).to(self.device)
    
-        # update critic
-        self.critic_optimizer.zero_grad()
-        
         curr_Q = self.critic.forward(state_batch, action_batch)
         next_actions = self.actor_target.forward(next_state_batch)
         next_Q = self.critic_target.forward(next_state_batch, next_actions.detach())
         expected_Q = reward_batch + self.gamma * next_Q
-        critic_loss = self.MSELoss(curr_Q, expected_Q.detach())
-        critic_loss.backward() 
+        
+        # update critic
+        q_loss = F.mse_loss(curr_Q, expected_Q.detach())
+
+        self.critic_optimizer.zero_grad()
+        q_loss.backward() 
         self.critic_optimizer.step()
 
         # update actor
-        self.actor_optimizer.zero_grad()
         policy_loss = -self.critic.forward(state_batch, self.actor.forward(state_batch)).mean()
+        
+        self.actor_optimizer.zero_grad()
         policy_loss.backward()
         self.actor_optimizer.step()
 
