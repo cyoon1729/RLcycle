3a9df122bcaf599ec10a43fc61593bfbd3eed9a9
chris <chrisyoon1729@gmail.com>
2019-07-19 15:01:22 -0400

    comments

--- a2c/a2c.py
+++ a2c/a2c.py
@@ -36,27 +36,33 @@ class A2CAgent():
         next_states = torch.FloatTensor([sars[3] for sars in trajectory]).to(self.device)
         dones = torch.FloatTensor([sars[4] for sars in trajectory]).view(-1, 1).to(self.device)
         
+        # compute discounted rewards
         discounted_rewards = [torch.sum(torch.FloatTensor([self.gamma**i for i in range(rewards[j:].size(0))])\
              * rewards[j:]) for j in range(rewards.size(0))]  # sorry, not the most readable code.
         value_targets = rewards.view(-1, 1) + torch.FloatTensor(discounted_rewards).view(-1, 1).to(self.device)
         
         logits, values = self.model.forward(states)
-        value_loss = F.mse_loss(values, value_targets.detach())
         dists = F.softmax(logits, dim=1)
         probs = Categorical(dists)
         
+        # compute value loss
+        value_loss = F.mse_loss(values, value_targets.detach())
+        
+        
+        # compute entropy bonus
         entropy = []
         for dist in dists:
             entropy.append(-torch.sum(dist.mean() * torch.log(dist)))
         entropy = torch.stack(entropy).sum()
         
+        # compute policy loss
         advantage = value_targets - values
         policy_loss = -probs.log_prob(actions.view(actions.size(0))).view(-1, 1) * advantage.detach()
         policy_loss = policy_loss.mean()
         
-        total_loss = policy_loss + 0.5 * value_loss - 0.001 * entropy 
+        total_loss = policy_loss + value_loss - 0.001 * entropy 
         return total_loss
-    
+        
     def update(self, trajectory):
         loss = self.compute_loss(trajectory)
 
