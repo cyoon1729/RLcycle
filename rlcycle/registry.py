3690ce9ff819a69d1b84f5324c9a965719fe60fd
Chris <chrisyoon1729@gmail.com>
2020-06-07 09:34:51 -0400

    add loss functions to registry

--- rlcycle/registry.py
+++ rlcycle/registry.py
@@ -1,13 +1,24 @@
-from rlcycle.a3c.agent import A3CAgent
-from rlcycle.ddpg.agent import DDPGAgent
-from rlcycle.dqn_base.agent import DQNAgent
-from rlcycle.sac.agent import SACAggent
-from rlcycle.td3.agent import TD3Agent
+from rlcycle.dqn_base.agent import DQNBaseAgent
+from rlcyle.dqn_base.loss import C51Loss, DQNLoss, QRLoss
+
+# from rlcycle.a3c.agent import A3CAgent
+# from rlcycle.ddpg.agent import DDPGAgent
+# from rlcycle.dqn_base.agent import DQNAgent
+# from rlcycle.sac.agent import SACAggent
+# from rlcycle.td3.agent import TD3Agent
+
 
 AGENTS = {
     "DQN": DQNAgent,
-    "DDPG": DDPGAgent,
-    "TD3": TD3Agent,
-    "A3C": A3CAgent,
-    "SAC": SACAgent,
+    # "Rainbow": DQNAgent
+    # "DDPG": DDPGAgent,
+    # "TD3": TD3Agent,
+    # "A3C": A3CAgent,
+    # "SAC": SACAgent,
+}
+
+LOSSES = {
+    "DQN": DQNLoss,
+    "C51": QRLoss,
+    "QR": C51Loss,
 }
