23c4f88587a37ff43a0a1a88f118766d6d624248
Chris <chrisyoon1729@gmail.com>
2020-06-07 09:34:34 -0400

    create action selector base class

--- rlcycle/common/abstract/action_selector.py
+++ rlcycle/common/abstract/action_selector.py
@@ -2,12 +2,16 @@ from abc import ABC, abstractmethod
 from typing import Tuple
 
 import numpy as np
+import torch
 import torch.nn as nn
 
 
 class ActionSelector(ABC):
     """Abstract base class for callable action selection methods"""
 
+    def __init__(self, device: torch.device):
+        self.deice = device
+
     @abstractmethod
-    def __call__(self, network: nn.Module, state: np.ndarray) -> Tuple[np.ndarray, ...]:
+    def __call__(self, policy: nn.Module, state: np.ndarray) -> Tuple[np.ndarray, ...]:
         pass
