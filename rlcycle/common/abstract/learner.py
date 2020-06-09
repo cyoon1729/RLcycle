48cc3a3e5116c20864bc264619ef298824fc4345
Chris <chrisyoon1729@gmail.com>
2020-06-09 10:40:27 -0400

    add abstract classes for wrapper and learnerbase

--- rlcycle/common/abstract/learner.py
+++ rlcycle/common/abstract/learner.py
@@ -4,7 +4,19 @@ from typing import Tuple
 import torch
 
 
-class Learner:
+class LearnerBase(ABC):
+    """Abstract class for Learner"""
+
+    @abstractmethod
+    def update_model(
+        self, experience: Tuple[torch.Tensor, ...]
+    ) -> Tuple[torch.Tensor, ...]:
+        pass
+
+
+class Learner(LearnerBase):
+    """Abstract class for all """
+
     def __init__(self, args: dict, hyper_params: dict, model_cfg: dict):
         self.args = args
         self.hyper_params = hyper_params
@@ -19,3 +31,16 @@ class Learner:
         self, experience: Tuple[torch.Tensor, ...]
     ) -> Tuple[torch.Tensor, ...]:
         pass
+
+
+class LearnerWrapper(LearnerBase):
+    """AbstractClass for Learner Wrappers"""
+
+    def __init__(self, learner: Learner):
+        self.learner = learner
+
+    @abstractmethod
+    def update_model(
+        self, experience: Tuple[torch.Tensor, ...]
+    ) -> Tuple[torch.Tensor, ...]:
+        pass    
