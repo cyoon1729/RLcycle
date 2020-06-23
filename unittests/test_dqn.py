c1aed39ce68bb21607d1150b92464b83dbdf3ad5
cyoon1729 <chrisyoon1729@gmail.com>
2020-06-23 18:15:02 +0900

    Dueling DQN lunar lander working configs

--- unittests/test_dqn.py
+++ unittests/test_dqn.py
@@ -6,7 +6,7 @@ from rlcycle.build import (build_action_selector, build_agent, build_env,
                            build_learner, build_loss, build_model)
 
 
-@hydra.main(config_path="../configs/meta_config.yaml", strict=False)
+@hydra.main(config_path="../configs/dqn.yaml", strict=False)
 def main(cfg: DictConfig):
     agent = build_agent(**cfg)
     agent.train()
